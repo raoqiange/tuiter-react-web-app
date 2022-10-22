92f733af48393e900efd35f69114c1f3ad80cd00
Rachel Rao <rao.q@northeastern.edu>
2022-10-22 15:47:06 -0700

    lab7

---
+++ src/labs/a7/redux-examples/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import HelloReduxExampleComponent from "./hello-redux-example-component";
+import {Provider} from "react-redux";
+import hello from "./reducers/hello";
+import todos from "./reducers/todos-reducer";
+// import {createStore} from "redux";
+import {configureStore} from "@reduxjs/toolkit";
+import Todos from "./todos-component";
+
+const store = configureStore({
+    reducer: {hello, todos}
+})
+
+const ReduxExamples = () => {
+    return(
+        <Provider store={store}>
+            <div>
+                <h2>Redux Examples</h2>
+                <Todos />
+                <HelloReduxExampleComponent />
+            </div>
+        </Provider>
+    );
+};
+
+export default ReduxExamples;
