11e57e07ff67f1eebef838a26912cdd7acc124e0
Rachel Rao <rao.q@northeastern.edu>
2022-11-10 01:46:32 -0800

    A8 update

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -4,7 +4,6 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 // console.log('env var' ,process.env.REACT_APP_API_BASE)
 const TUITS_API = `${API_BASE}/tuits`;
 
-
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit);
     return response.data;
